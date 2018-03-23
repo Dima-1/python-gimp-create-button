d1883f2d26051daea979c39652d18f76f4c802ba
Dima-1 <softmail32@gmail.com>
2018-03-23 23:20:32 +0300

    Add common layers name.

--- python_fu_create_button.py
+++ python_fu_create_button.py
@@ -2,33 +2,28 @@
 
 from gimpfu import *
 
-def create_button_l(img, layer) :
-    #img = gimp.image_list()[0]
 
-    drw = pdb.gimp_image_active_drawable(img)
-
+def create_button(img, layer, name):
     prev_layer = img.active_layer
     bounds = pdb.gimp_selection_bounds(img)
-    width = bounds[3] - bounds[1]
+    width = bounds.x2 - bounds.x1
     height = bounds[4] - bounds[2]
-    pdb.gimp_message(width)
-    pdb.gimp_message(height)
     outimg = pdb.gimp_image_new(width, height, RGB)
-    layer = pdb.gimp_layer_new(outimg, width, height, RGB_IMAGE, "Back", 100, NORMAL_MODE)
+    layer = pdb.gimp_layer_new(outimg, width, height, RGBA_IMAGE, "Background", 100, NORMAL_MODE)
     pdb.gimp_image_insert_layer(outimg, layer, None, 0)
 
     pdb.gimp_edit_copy(img.layers[0])
     fsel = pdb.gimp_edit_paste(layer, False)
     pdb.gimp_floating_sel_to_layer(fsel)
-    outimg.active_layer.name = "new.up"
+    outimg.active_layer.name = name + ".up"
     pdb.gimp_edit_copy(img.layers[1])
     fsel = pdb.gimp_edit_paste(layer, False)
     pdb.gimp_floating_sel_to_layer(fsel)
-    outimg.active_layer.name = "new.over"
+    outimg.active_layer.name = name + ".over"
     pdb.gimp_edit_copy(img.layers[2])
     fsel = pdb.gimp_edit_paste(layer, False)
     pdb.gimp_floating_sel_to_layer(fsel)
-    outimg.active_layer.name = "new.down"
+    outimg.active_layer.name = name + ".down"
     outimg.remove_layer(outimg.layers[3])
 
     img.active_layer = prev_layer
@@ -38,16 +33,16 @@ def create_button_l(img, layer) :
 
 
 register(
-    "python_fu_create_button_l",
+    "python_fu_create_button",
     "Create button",
-    "Create button from pile of layer",
+    "Create button from pile of layers",
     "DR",
     "MIT License",
     "2018",
-    "<Image>/Test/Create button",
+    "<Image>/Script/Create button...",
     "*",
+    [(PF_STRING, "name", "Layers name", "button")],
     [],
-    [],
-    create_button_l)
+    create_button)
 
 main()
