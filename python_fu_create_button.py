9008a582a168c663807b2b0e1c1fc512447c36d0
Dima-1 <softmail32@gmail.com>
2018-03-23 23:33:52 +0300

    Add check if area selected

--- python_fu_create_button.py
+++ python_fu_create_button.py
@@ -5,31 +5,34 @@ from gimpfu import *
 
 def create_button(img, layer, name):
     prev_layer = img.active_layer
-    bounds = pdb.gimp_selection_bounds(img)
-    width = bounds.x2 - bounds.x1
-    height = bounds[4] - bounds[2]
-    outimg = pdb.gimp_image_new(width, height, RGB)
-    layer = pdb.gimp_layer_new(outimg, width, height, RGBA_IMAGE, "Background", 100, NORMAL_MODE)
-    pdb.gimp_image_insert_layer(outimg, layer, None, 0)
+    non_empty,x1,y1,x2,y2 = pdb.gimp_selection_bounds(img)
+    if non_empty:
+        width = x2 - x1
+        height = y2 - y1
+        outimg = pdb.gimp_image_new(width, height, RGB)
+        layer = pdb.gimp_layer_new(outimg, width, height, RGBA_IMAGE, "Background", 100, NORMAL_MODE)
+        pdb.gimp_image_insert_layer(outimg, layer, None, 0)
 
-    pdb.gimp_edit_copy(img.layers[0])
-    fsel = pdb.gimp_edit_paste(layer, False)
-    pdb.gimp_floating_sel_to_layer(fsel)
-    outimg.active_layer.name = name + ".up"
-    pdb.gimp_edit_copy(img.layers[1])
-    fsel = pdb.gimp_edit_paste(layer, False)
-    pdb.gimp_floating_sel_to_layer(fsel)
-    outimg.active_layer.name = name + ".over"
-    pdb.gimp_edit_copy(img.layers[2])
-    fsel = pdb.gimp_edit_paste(layer, False)
-    pdb.gimp_floating_sel_to_layer(fsel)
-    outimg.active_layer.name = name + ".down"
-    outimg.remove_layer(outimg.layers[3])
+        pdb.gimp_edit_copy(img.layers[0])
+        fsel = pdb.gimp_edit_paste(layer, False)
+        pdb.gimp_floating_sel_to_layer(fsel)
+        outimg.active_layer.name = name + ".up"
+        pdb.gimp_edit_copy(img.layers[1])
+        fsel = pdb.gimp_edit_paste(layer, False)
+        pdb.gimp_floating_sel_to_layer(fsel)
+        outimg.active_layer.name = name + ".over"
+        pdb.gimp_edit_copy(img.layers[2])
+        fsel = pdb.gimp_edit_paste(layer, False)
+        pdb.gimp_floating_sel_to_layer(fsel)
+        outimg.active_layer.name = name + ".down"
+        outimg.remove_layer(outimg.layers[3])
 
-    img.active_layer = prev_layer
+        img.active_layer = prev_layer
 
-    pdb.gimp_display_new(outimg)
-    pdb.gimp_displays_flush()
+        pdb.gimp_display_new(outimg)
+        pdb.gimp_displays_flush()
+    else:
+        pdb.gimp_message("Please select area for button")
 
 
 register(
