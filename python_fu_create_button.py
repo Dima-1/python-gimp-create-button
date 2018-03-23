2aafa47ffb34d6d0230d724c6a3679cb8837716b
Dima-1 <softmail32@gmail.com>
2018-03-23 22:50:54 +0300

    Add three layers for "up", "down", "over" states.

--- python_fu_create_button.py
+++ python_fu_create_button.py
@@ -4,31 +4,39 @@ from gimpfu import *
 
 def create_button_l(img, layer) :
     #img = gimp.image_list()[0]
-    
+
     drw = pdb.gimp_image_active_drawable(img)
-    
+
     prev_layer = img.active_layer
-    pdb.gimp_edit_copy(img.active_layer)
     bounds = pdb.gimp_selection_bounds(img)
-    width=bounds[3]-bounds[1]
-    height=bounds[4]-bounds[2]
+    width = bounds[3] - bounds[1]
+    height = bounds[4] - bounds[2]
     pdb.gimp_message(width)
     pdb.gimp_message(height)
-    outImg = pdb.gimp_image_new(width, height, RGB)
-    layer = pdb.gimp_layer_new(outImg, width, height, RGB_IMAGE, "Back", 100, NORMAL_MODE)
-    pdb.gimp_image_insert_layer(outImg, layer, None, 0)
-    
+    outimg = pdb.gimp_image_new(width, height, RGB)
+    layer = pdb.gimp_layer_new(outimg, width, height, RGB_IMAGE, "Back", 100, NORMAL_MODE)
+    pdb.gimp_image_insert_layer(outimg, layer, None, 0)
 
+    pdb.gimp_edit_copy(img.layers[0])
     fsel = pdb.gimp_edit_paste(layer, False)
     pdb.gimp_floating_sel_to_layer(fsel)
-    outImg.active_layer.name = "new.up"
+    outimg.active_layer.name = "new.up"
+    pdb.gimp_edit_copy(img.layers[1])
+    fsel = pdb.gimp_edit_paste(layer, False)
+    pdb.gimp_floating_sel_to_layer(fsel)
+    outimg.active_layer.name = "new.over"
+    pdb.gimp_edit_copy(img.layers[2])
+    fsel = pdb.gimp_edit_paste(layer, False)
+    pdb.gimp_floating_sel_to_layer(fsel)
+    outimg.active_layer.name = "new.down"
+    outimg.remove_layer(outimg.layers[3])
+
     img.active_layer = prev_layer
-    
-    pdb.gimp_invert(layer)
-    display = pdb.gimp_display_new(outImg)
+
+    pdb.gimp_display_new(outimg)
     pdb.gimp_displays_flush()
-    
-    
+
+
 register(
     "python_fu_create_button_l",
     "Create button",
@@ -43,4 +51,3 @@ register(
     create_button_l)
 
 main()
- 
