8f8f3622f53b7ddcd63217c015a215ea8853f382
Dima-1 <softmail32@gmail.com>
2018-03-26 12:32:30 +0300

    Implement select a directory and choose a file or layer

--- python_fu_create_button.py
+++ python_fu_create_button.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import os
 from gimpfu import *
 
-def create_button(img, layer, name):
+current_path = os.getcwd()
+
+
+def create_button(img, layer, name, dist, path_name):
     prev_layer = img.active_layer
     non_empty, x1, y1, x2, y2 = pdb.gimp_selection_bounds(img)
     if non_empty:
@@ -19,15 +23,20 @@ def create_button(img, layer, name):
             pdb.gimp_edit_copy(img.layers[index])
             pdb.gimp_floating_sel_to_layer(pdb.gimp_edit_paste(layer, False))
             out_img.active_layer.name = name + suffix[index]
+            if dist == 1:
+                file_name = name + suffix[index] + ".png"
+                full_path = os.path.join(path_name, file_name)
+                pdb.file_png_save_defaults(out_img, out_img.active_layer, full_path,
+                                           file_name)
             index = index + 1
         out_img.remove_layer(out_img.layers[len(out_img.layers) - 1])
-
         img.active_layer = prev_layer
-
-        pdb.gimp_display_new(out_img)
-        pdb.gimp_displays_flush()
+        if dist == 0:
+            pdb.gimp_display_new(out_img)
+            pdb.gimp_displays_flush()
     else:
         pdb.gimp_message("Please select area for button")
+
 
 register(
     "python_fu_create_button",
@@ -39,7 +48,11 @@ register(
     "2018",
     "<Image>/Script/Create button...",
     "*",
-    [(PF_STRING, "name", "Common name of layers", "button")],
+    [
+        (PF_STRING, "name", "Common name of layers", "button"),
+        (PF_RADIO, "dist", "Save to:", 0, (("layers", 0), ("files", 1))),
+        (PF_DIRNAME, "path_name", "Output directory", current_path)
+    ],
     [],
     create_button)
 
